3e97e6781690a2e562159e0b9e36bf0d6f0ed4fd
padams <ghusiyy@gmail.com>
2020-07-18 17:21:12 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let lessons = [
+    { title: "", days: [], texts: [{ title: "", links: [{}] }], time: null }
+  ];
+</script>
+
+<main>Schedule for 26th Annual Dowra Classes</main>
+<div>From July 11th to August 19th</div>
+<div class="classes">
+  <div>Fajr Lesson</div>
+  <div>Rawhah Lesson</div>
+  <div>Sirah Lesson</div>
+  <div>Tafsir Lesson</div>
+  <div>Qabas al-Nur al-Mubin Lesson</div>
+  <div>Irshadat al-Suluk</div>
+</div>
